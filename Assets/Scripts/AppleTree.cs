a826dd94ce9b8be47c960663545c0538aafe9cb1
ControllerPrime <31867665+ControllerPrime@users.noreply.github.com>
2018-09-11 13:31:39 -0400

    C# for AppleTree
    Added C# script for the Apple Tree

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour {
+    [Header("Set in Inspector")]
+
+    public GameObject applePrefab;
+
+    public float speed = 1f;
+
+    public float leftAndRightEdge = 10f;
+
+    public float changeToChangeDirections = 0.1f;
+
+    public float secondsBetweenAppleDrops = 1f;
+
+    private void Start()
+    {
+        //Dropping apples every second
+    }
+
+    private void Update()
+    {
+        //Basic Movement
+        //Changing Direction
+    }
+}
